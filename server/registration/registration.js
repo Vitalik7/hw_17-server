fd79e30523f19401200d116797c3778286abd1c7
Vitaliy <vitaliy@MacBook-Pro-Vitaliy.local>
2018-03-14 12:28:50 +0200

    fix registration

--- server/registration/registration.js
+++ server/registration/registration.js
@@ -1,44 +1,67 @@
 const express = require('express')
-const bcrypt = require('bcrypt')
 
-const User = require('./model')
+const User = require('/model')
 
 const router = express.Router()
 
-router.post('/login',
-    async function (req, res, next) {
-      if (!req.body.user.password || !req.body.user.email) {
-        res.json({'status': 404, 'message': 'Not Found.'})
-      }
-      let user = await User.findOne({email: req.body.user.email})
-        .then(user => user)
+router.get('/user/:id', (req, res) => {
+  User.findById({'_id': req.params.id}, (err, user) => {
+    if (err) {
+      res.status(400).json(err)
+    }
+    res.status(200).json(user)
+  })
+})
 
-      if (!user) {
-        res.json({'status': 404, 'message': 'Not Found.'})
-      }
+router.get('/user', (req, res, next) => {
+  User.find({})
+    .then(user => {
+      res.json({user})
+    })
+    .catch(next)
+})
 
-      bcrypt.compare(req.body.user.password, user.password, function (err, result) {
-        if (result === true) {
-          res.json({'user': {'_id': user._id, 'email': user.email, 'name': user.name}})
-        } else {
-          res.json({'status': 404, 'message': 'Not Found.'})
-        }
+router.post('/user', (req, res) => {
+  let newUser = new User(req.body.user)
+  if (req.body.user.email !== '' && req.body.user.password !== '' && req.body.user.username !== '') {
+    newUser.save()
+      .then(user => {
+        res.json({user})
+      }, (err) => {
+        res.status(400).json(err)
       })
-    })
+  } else {
+    console.log('user err , field is empty : ', req.body.user)
+    res.status(400).json({success: false, message: 'Please fill in the fields'})
+  }
+})
 
-router.post('/users', (req, res, next) => {
-  let data = req.body.user
+router.put('/user/:id', function (req, res) {
+  let user = req.body.user
+  if (('email' in user) && ('password' in user) && ('username' in user)) {
+    User.findOneAndUpdate({'_id': req.params.id},
+      {email: req.body.user.email,
+        password: req.body.user.password,
+        username: req.body.user.username
+      },
+      {new: true},
+      function (err, user) {
+        if (err) {
+          res.status(400).json(err)
+        }
+        res.status(200).json(user)
+      })
+  } else {
+    res.status(400).json({success: false, message: 'unacceptable changes'})
+  }
+})
 
-  bcrypt.genSalt(10, function (err, salt) {
-    bcrypt.hash(data.password, salt, function (err, hash) {
-      data.password = hash
-      new User(data)
-            .save()
-            .then(user => {
-              res.json({user})
-            })
-            .catch(next)
-    })
+router.delete('/user/:id', function (req, res) {
+  let id = req.params.id
+  User.remove({
+    _id: id
+  }, function () {
+    res.json()
   })
 })
 
