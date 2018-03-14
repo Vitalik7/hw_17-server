8dcc9539aae8d97515dacf4f6afac45b8b965b96
Vitaliy <vitaliy@MacBook-Pro-Vitaliy.local>
2018-03-14 12:22:55 +0200

    fix bug

--- server/app.js
+++ server/app.js
@@ -1,28 +1,57 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = require('express')()
 const http = require('http').Server(app)
+const bodyParser = require('body-parser')
 const io = require('socket.io')(http)
-const path = require('path')
-const MessageModel = require('./chat/model')
-const message = require('./chat/chat')
+const MessageModel = require('./message/model')
+const message = require('./message/message')
+const passport = require('passport')
+const FacebookStrategy = require('passport-facebook')
+const facebook = config.facebook
 
 const config = require('./config')
 const user = require('./registration/registration')
 
-const app = express()
-
+const app = require('express')()
 
 require('./db')
-require('./auth')
 
-app.listen(config.port, () => {
+http.listen(config.port, () => {
   console.log(`Server running at port: ${config.port}`)
 })
+
+const transformFacebookProfile = (profile) => ({
+  name: profile.name,
+  avatar: profile.picture.data.url
+})
+
+passport.use(new FacebookStrategy(facebook,
+    // Gets called when user authorizes access to their profile
+    async function (accessToken, refreshToken, profile, done) {
+      done(null, transformFacebookProfile(profile._json))
+    }
+))
+
+// Serialize user into the sessions
+passport.serializeUser((user, done) => done(null, user))
+
+// Deserialize user from the sessions
+passport.deserializeUser((user, done) => done(null, user))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(bodyParser.json())
+app.use('/api', message)
+app.use('/api', user)
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
+
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/auth/facebook' }),
+    // Redirect user back to the mobile app using Linking with a custom protocol OAuthLogin
+    (req, res) => {
+      res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user))
+    })
 
 io.on('connection', function (socket) {
   console.log('a user connected')
@@ -39,10 +68,6 @@ io.on('connection', function (socket) {
     console.log('user disconnected')
   })
 })
-
-app.use(bodyParser.json())
-
-app.use('/api/v1', user)
 
 // error handling
 app.use((req, res, next) => {
